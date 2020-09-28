832f0ff672e69940a0c97003a0a9d8c2a244918c
Shiyao W <62321876+S-0w0-W@users.noreply.github.com>
2020-09-29 00:34:00 +1300

    Update LinkedList.h

--- Pathfinding/Pathfinding/LinkedList.h
+++ Pathfinding/Pathfinding/LinkedList.h
@@ -2,39 +2,40 @@
 
 struct Node {
 public:
-	int data;
-	Node* next;
-	Node* prev;
+    int data;
+    Node* next;
+    Node* prev;
 
-	void setPrev(Node* x) {
-		prev = x;
-	}
+    void setPrev(Node* x) {
+        prev = x;
+    }
 
-	void setNext(Node* x) {
-		next = x;
-	}
+    void setNext(Node* x) {
+        next = x;
+    }
 
-	void setData(int x) {
-		data = x;
-	}
+    void setData(int x) {
+        data = x;
+    }
 };
 
 class LinkedList
 {
 private:
-	Node* head, * tail;
+    Node* head, * tail;
 public:
-	LinkedList();				// Constructor
-	void Insert(int data);		// Inserts a integer to the list
-	Node* Dequeue();				// Dequeue the head of the list and returns the data of the head
-	Node* Head();				// Returns the head Node
-	Node* Tail();				// Returns the tail Node
-	Node* Next(Node* x);		// Returns the next Node of the input node
-	Node* Prev(Node* x);		// Returns the previous Node of the input node
-	int Size();					// Returns the size of the list
-	void Print();				// Prints out data of list
-	int Find(int x);			// Returns the index(starting from 1) of input integer, returns -1 if not found
-	int* Return(Node* x);		// Returns a pointer to array of integer of node data back to the start starting from input node
-	Node* Pop(int x);			// Returns a Pointer to the first Node which has the data of input integer starting from head
-	Node* Popindx(int x);			// Returns a Pointer to the Node which has the data of input integer
+    LinkedList();                // Constructor
+    void Insert(int data);        // Inserts a integer to the list
+    Node* DequeueHead();        // Dequeue the head of the list and returns the data of the head
+    Node* DequeueTail();        // Dequeue the Tail of the list and returns the data of the Tail
+    Node* Head();                // Returns the head Node
+    Node* Tail();                // Returns the tail Node
+    Node* Next(Node* x);        // Returns the next Node of the input node
+    Node* Prev(Node* x);        // Returns the previous Node of the input node
+    int Size();                    // Returns the size of the list
+    void Print();                // Prints out data of list
+    int Find(int x);            // Returns the index(starting from 1) of input integer, returns -1 if not found
+    int* Return(Node* x);        // Returns a pointer to array of integer of node data back to the start starting from input node
+    Node* Pop(int x);            // Returns a Pointer to the first Node which has the data of input integer starting from head
+    Node* Popindx(int x);            // Returns a Pointer to the Node which has the data of input integer
 };
